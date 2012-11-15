35b345434b8e4f6ccced2e984657592fd1bd2b64
Arup Sen <arup@arupdesigns.co.uk>
2012-11-15 12:14:57 +0000

    first commit

---
+++ parents_menu.php
@@ -0,0 +1,26 @@
+<?php 
+/*
+-- school menu which appears only for pages in parents
+*/
+?>
+    <ul id="parents_menu">
+  <li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=7">parent pages</a></li>
+    <li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=124">pta</a></li>
+    <li><a>term dates</a></li>
+    <li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=23">lunch</a></li>
+    <li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=25">uniform</a></li>
+    <li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=2018">attendance &amp; punctuality</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=17">homework</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=126">parents seeing teachers</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=96">play activities at lunchtime</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=14">clubs &amp; activities</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=24">breakfast club &amp; after school care</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=1782">moving on to secondary school</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=256">internet safety</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=481">travel plan</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=217">celebrating birthdays</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=97">policies</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=2476">collective worship</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=19">behaviour</a></li>
+    </ul>
+
---
+++ school_menu.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+-- school menu which appears only for pages in our school
+*/
+?>
+    <ul id="school_menu">
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=2">history</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=9">staff</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=10">governors</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=42">aims &amp; values</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=97">policies</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=4266">community projects</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=20">admissions</a></li>
+	<li><a>prospectus</a></li>
+	<li><a href="http://schoolsfinder.direct.gov.uk/2122584/overview/" target="_blank" >ofsted report</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=3060">job vacancies</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?cat=15">Celebrations</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page=175">School Council</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page=173">Freedom of Information</a></li>
+	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page=1302">Fair Processing Notice</a></li>
+    </ul>
+
