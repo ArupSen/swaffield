4d75be1a49594ff2b4c913e9361ade143854feeb
Arup Sen <arup@arupdesigns.co.uk>
2012-11-16 23:46:45 +0000

    removed the word Testing

--- newsBox.php
+++ newsBox.php
@@ -4,9 +4,9 @@
 ?>
 
 <div id="newsBox">
-<h2>Quick Links - Testing</h2>
+<h2>Quick Links</h2>
 <ul>
-<?php wp_list_bookmarks('title_li=0&category=20&categorize=0&orderby=notes'); ?>
+<?php wp_list_bookmarks('title_li=0&category=20&categorize=0'); ?>
 <!--
 <li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=29" title="">What's on This Week</a></li>
 <li><a href="" title="">Dates for your Diary</a></li>
