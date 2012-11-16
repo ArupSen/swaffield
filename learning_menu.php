416f51ab8ea589f12cdaa48da180b8790295de84
Arup Sen <arup@arupdesigns.co.uk>
2012-11-16 23:48:03 +0000

    removed links and replaced with wp_list_bookmarks call

--- learning_menu.php
+++ learning_menu.php
@@ -1,33 +1,9 @@
 <?php 
 /*
--- school menu which appears only for pages in learning
+-- learning menu which appears only for pages in learning
 */
 ?>
     <ul id="learning_menu">
-    <li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=26">general information</a></li>
-    <li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=4854">nursery</a></li>
-    <li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=4942">reception</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=4903">year 1</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=4932">year 2</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=4839">Year 3</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=4939">Year 4</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=4890">Year 5</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=4818">Year 6</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=171">Reading and Books</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=130">English</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=131">Maths</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=132">Science</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=133">ICT</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=134">The Arts</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=821">PE</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=243">Fun &amp; Educational websites</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=115">Special Educational Needs</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=116">Assesment and Monitoring</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=111">Foundation Stage</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=112">Key Stage 1</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=113">Key Stage 2</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=2476">Collective Worship</a></li>
-    	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=121">Curriculum</a></li>
-    	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=5043">Links and Resources</a></li>
+	<?php wp_list_bookmarks('title_li=&category=19&categorize=0'); ?>
     </ul>
 
--- school_menu.php
+++ school_menu.php
@@ -4,19 +4,6 @@
 */
 ?>
     <ul id="school_menu">
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=2">history</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=9">staff</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=10">governors</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=42">aims &amp; values</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=97">policies</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=4266">community projects</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=20">admissions</a></li>
-	<li><a>prospectus</a></li>
-	<li><a href="http://schoolsfinder.direct.gov.uk/2122584/overview/" target="_blank" >ofsted report</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page_id=3060">job vacancies</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?cat=15">Celebrations</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page=175">School Council</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page=173">Freedom of Information</a></li>
-	<li><a href="http://www.swaffield.wandsworth.sch.uk/beta-site/?page=1302">Fair Processing Notice</a></li>
+	<?php wp_list_bookmarks('title_li=&category=21&categorize=0'); ?>
     </ul>
 
